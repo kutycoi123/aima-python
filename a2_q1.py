ed59c961b45dedd741db7c2ee4b2e7f996012b45
tln3 <tln3@sfu.ca>
2020-06-07 20:54:34 -0700

    Fixed rand_graph

--- a2_q1.py
+++ a2_q1.py
@@ -3,13 +3,13 @@ def rand_graph(p, n):
     graph = {}
     CONNECTED = 1
     NOT_CONNECTED = 0
-    choice = [CONNECTED, NOT_CONNECTED] # 0 means not-connected, 1 means connected
-    for node_begin in range(n):
-        graph[node_begin] = [] 
-        for node_end in range(n):
-            if node_begin != node_end:
-                rand_choice = choices(choice, [p, 1-p])
-                if rand_choice[0] == CONNECTED:
-                    graph[node_begin].append(node_end)
+    for node in range(n):
+        graph[node] = []
+    for node in range(n):
+        for neighbor in range(node+1, n):
+            rand_choice = choices([CONNECTED, NOT_CONNECTED], [p, 1-p])
+            if rand_choice[0] == CONNECTED:
+                graph[node].append(neighbor)
+                graph[neighbor].append(node)
     return graph
 
